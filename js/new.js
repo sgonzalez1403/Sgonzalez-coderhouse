44337682fbd36f07a883e67934715f801b89d2ed
sgonzalez1403 <116124121+sgonzalez1403@users.noreply.github.com>
2023-01-19 20:35:07 -0300

    Update new.js

--- js/new.js
+++ js/new.js
@@ -1,133 +1,70 @@
 
-function menuPrincipal() {
-let total= 0
-let menu
-do {
-  menu = prompt("1- Armar mi PC \n\n  2-Cerrar cuenta \n\n 3-Salir");
-  menu = parseInt(menu);
-  switch (menu) {
-    case 1:
-      total = total + tarjetasGraficas() + placasMadre() + Procesadores() + Ram() ;
-      break;
-    case 2:
-      alert ("el total acumulado "+ total);
-      total= 0
-      break;
-      case 3: alert ("Saliendo")
-      break
-  }
-} while (menu != 3);
-}
-
-function tarjetasGraficas() {
-  let acumulador = 0;
-  alert("Seleccione su tarjeta gráfica");
-  let tipo = prompt("Ingrese el modelo de tarjeta gráfica que desea utilizar \n\n 1-RTX3070 \n\n  2-RTX3060 \n\n  3-RTX3050 \n\n  4-GTX1660 \n\n 5-Salir");
-  tipo = parseInt(tipo);
-
-  switch (tipo) {
-    case 1:
-      acumulador = 300000;
-      break;
-
-    case 2:
-      acumulador = 250000;
-      break;
-
-    case 3:
-      acumulador = 200000;
-      break;
-    case 4:
-      acumulador = 150000;
-      break;
-      case 5: alert ("Saliendo")
+alert ("Elegi los componentes de tu computadora")
     
-  }
-  return acumulador
+
+const componentespc= [
+
+{producto:"procesador", marca: "AMD ryzen 5", precio: 50000 },
+{producto:"gráfica", marca: "RTX3050", precio: 60000},
+{producto:"Ram", marca: "Ram Kingston", precio: 20000},
+{producto:"Gabinete", marca: "Corsair", precio: 10000},
+{producto:"Fuente", marca: "coolermaster", precio: 20000},
+{producto:"placa madre", marca: "Placa madre Asus", precio: 25000},
+]
+
+const carrito= []
+
+let propuesta = prompt ( "¿Desea comprar algun producto?")
+
+while ( propuesta != "si" && propuesta != "no") {
+
+    alert ("Responda por si o por no")
+    propuesta = prompt ("¿Deseas comprar algun producto? (Si o No)")
 }
 
+if  (propuesta == "si") {
 
-function placasMadre(){
+    alert ("Puede ver los productos a continuación")
+    let todosLosProductos= componentespc.map (
+        (comp) => comp.producto + " " + comp.precio + "$"
+    );
 
-  let acumulador = 0;
-  alert("Seleccione su placa madre");
-  let tipo = prompt("Ingrese el modelo de Placa madre que desea utilizar \n\n  1-Asus A320M-K \n\n  2-Gigabyte GA-A320M-H \n\n  3-ASUS PRO A320M-R WI-FI/CSM \n\n 4-salir");
-  tipo = parseInt(tipo);
+    alert (todosLosProductos.join(" - "))
 
-  switch (tipo) {
-    case 1:
-      acumulador = 15000;
-      break;
-
-    case 2:
-      acumulador = 20000;
-      break;
-
-    case 3:
-      acumulador = 25000;
-      break;
-
-    case 4: alert ("Saliendo")
-      
-   
-  }
-  return acumulador
-
-  
 
 }
 
-function Procesadores(){
+else if (propuesta == "no") {
 
-  let acumulador = 0;
-  alert("Seleccione su procesador");
-  let tipo = prompt("Ingrese el procesador que desea utilizar \n\n  1-Ryzen 3 \n\n  2-Ryzen 5 \n\n  3-Ryzen 7 \n\n 4-Salir");
-  tipo = parseInt(tipo);
+    alert ("Muchas gracias vuelva prontos")
 
-  switch (tipo) {
-    case 1:
-      acumulador = 40000;
-      break;
-
-    case 2:
-      acumulador = 50000;
-      break;
-
-    case 3:
-      acumulador = 60000;
-      break;
-
-    case 4: alert ("Saliendo")
-
-   
-  }
-  return acumulador
-}
-function Ram(){
-
-  let acumulador = 0;
-  alert("Seleccione cantidad de ram");
-  let tipo = prompt("Ingrese la cantidad de ram que desea utilizar \n\n  1-16gb \n\n  2-32GB \n\n  3-64GB \n\n 4-Salir");
-  tipo = parseInt(tipo);
-
-  switch (tipo) {
-    case 1:
-      acumulador = 40000;
-      break;
-
-    case 2:
-      acumulador = 50000;
-      break;
-
-    case 3:
-      acumulador = 60000;
-      break;
-
-    case 4: alert ("Saliendo")
-  
-    
-  }
-  return acumulador
 }
 
-menuPrincipal()
+while ( propuesta != "no") {
+    let comp = prompt ( "agrega un producto a tu carrito")
+
+    let productoEncontrado = componentespc.find ((item) => item.producto==comp )
+
+    if (productoEncontrado != undefined) {
+
+        let unidades = parseInt (prompt("¿Cuantas unidades quiere llevar?"))
+
+        carrito.push ({nombre:comp, cantidad: unidades, precio:productoEncontrado.precio})
+        console.log (carrito)
+    } else {
+        alert ("No tenemos ese producto")
+    }
+    propuesta = prompt ("¿Desea algun otro producto? si/no")
+
+    if (propuesta === "no") {
+
+    alert ("Gracias por su compra")
+    carrito.forEach ((final) => {
+        console.log (`comp: ${final.nombre}, unidades ${final.cantidad}, total a pagar por el producto ${final.cantidad * final.precio} `)
+    })
+}
+
+}
+
+const total = carrito.reduce ((acc,el)=> acc + el.precio * el.cantidad, 0) 
+console.log (`El total a pagar por su compra : ${total}`)
